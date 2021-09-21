e1482225832b82977f6c6a3b193c48e89ca04738
hugo1623 <hugoluis.system@gmail.com>
2021-09-21 17:30:20 -0500

    Creación de AutoresController

---
+++ WebApiLibrosAurores/Controllers/AutoresController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiLibrosAurores.Controllers
+{
+    [ApiController] //Permite hacer validaciones Automaticas respecto a la data recibida del controlador
+
+    [Route("api/autores")] // Permite definir las rutas para nuestros controladores
+
+    public class AutoresController: ControllerBase
+    {
+
+    }
+}
